49d56933d7d8afd7ee83f83d9ae0530560acbdcb
Djamel BENDAOUD <dbendaou@e2r5p15.42.fr>
2015-12-20 19:58:20 +0100

    debut de get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbendaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/20 19:50:17 by dbendaou          #+#    #+#             */
+/*   Updated: 2015/12/20 19:54:10 by dbendaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		GET_NEXT_LINE_H
+# define	GET_NEXT_LINE_H
+# define	BUF_SIZE
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+int 	get_next_line(int const fd, char ** line);
+size_t	ft_strlen(char const *str);
+
+#endif
